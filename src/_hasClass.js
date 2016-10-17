f068f327b3113fc3984c65a9f24955bcf6cd6960
Stefan Gabos <contact@stefangabos.ro>
2016-10-17 21:44:00 +0300

    Added hasClass method

--- src/_hasClass.js
+++ src/_hasClass.js
@@ -1,6 +1,38 @@
 /**
- *  @todo   Needs to be written!
+ *  Checks whether *any* of the matched elements have the given class.
+ *
+ *  > This method uses JavaScript's {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/classList classList.contains}
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var elements = $('selector');
+ *
+ *  // check if matched elements have a certain class
+ *  console.log(elements.hasClass('some-class'));
+ *
+ *  // since this method returns the set of matched elements, we can use chaining
+ *  elements.toggleClass('some-class');
+ *
+ *  @param  {string}    class_name  The name of a class to be checked if it exists on *any* of the elements in the set
+ *                                  of matched elements.
+ *
+ *  @todo               This method currently doesn't work on IE9
+ *
+ *  @return {boolean}   Returns TRUE if the sought class exists in *any* of the elements in the set of matched elements.
  */
-this.hasClass = function() {
+this.hasClass = function(class_name) {
+
+    var i, j;
+
+    // iterate through the matched elements
+    for (i in collection)
+
+        // if sought class exists, return TRUE
+        if (collection[i].classList.contains(class_name)) return true;
+
+    // return FALSE if we get this far
+    return false;
 
 }
