9fb6e607a23c2b31ee2d2ea93a8ea73eca66f2e6
Stefan Gabos <contact@stefangabos.ro>
2016-10-17 23:28:56 +0300

    Added the "text" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -757,9 +757,56 @@ $ = function(selector, parent, first_only) {
         }
 
         /**
-         *  @todo   Needs to be written!
+         *  Gets the text content of the first element in the set of matched elements (combined with the text content of all its
+         *  descendants), or sets the text contents of the matched elements.
+         *
+         *  > This method is a wrapper for JavaScript's {@link https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent textContent}
+         *
+         *  @example
+         *
+         *  // always cache selectors
+         *  // to avoid DOM scanning over and over again
+         *  var elements = $('selector');
+         *
+         *  // set the text content for all the matched elements
+         *  elements.text('Hello');
+         *
+         *  // get the text content of the first element in the
+         *  // set of matched elements (including its descendants)
+         *  var content = elements.text();
+         *
+         *  // since this method returns the set of matched elements,
+         *  // we can use chaining
+         *  elements.text('Hello').addClass('some-class');
+
+         *  @param  {string}    [content]   The text to set as the content of all the matched elements. Note that any text
+         *                                  content that was previously in that element is completely replaced by the new
+         *                                  content.
+         *
+         *  @return {$|string}              When the `content` argument is provided, this method returns the set of matched elements,
+         *                                  for chaining. Otherwise it returns the text content of the first element in the set
+         *                                  of matched elements (combined with the text content of all its descendants)
          */
-        this.text = function() {
+        this.text = function(content) {
+
+            var i;
+
+            // if content is provided
+            if (content)
+
+                // iterate through the matched elements
+                for (i in collection)
+
+                    // set the text content of each element
+                    collection[i].textContent = content;
+
+            // if content is not provided
+            // return the text content of the first element in the set of matched elements
+            // (combined with the text content of all its descendants)
+            else return collection[0].textContent;
+
+            // return the set of matched elements, for chaining
+            return $this;
 
         }
 
