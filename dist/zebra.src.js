2a9e65b6b79a7bf334859ecfb496af2d84da5035
Stefan Gabos <contact@stefangabos.ro>
2016-10-22 22:51:32 +0300

    Minor documentation updates for the "append" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -166,7 +166,12 @@ $ = function(selector, parent, first_only) {
         }
 
         /**
-         *  Inserts content, specified by the parameter, to the end of each element in the set of matched elements.
+         *  Inserts content, specified by the argument, to the end of each element in the set of matched elements.
+         *
+         *  Both this and the {@link $#appendTo .appendTo()} method perform the same task, the main difference being in the placement
+         *  of the content and the target. With `.append()`, the selector expression preceding the method is the container into
+         *  which the content is to be inserted. On the other hand, with `.appendTo()`, the content precedes the method, and it
+         *  is inserted into the target container.
          *
          *  > If there is more than one target element, clones of the inserted element will be created for each target except for
          *  the last one. For the last target, the original item will be inserted.
@@ -200,7 +205,7 @@ $ = function(selector, parent, first_only) {
          *  parent.append('<div>hello</div>');
          *
          *  // since this method returns the set of matched elements, we can use chaining
-         *  parent.append($('div')).addClass('appendedDiv');
+         *  parent.append($('div')).addClass('classname');
          *
          *  @param  {mixed}     content     DOM element, text node, HTML string, or ZebraJS object to insert at the end of each
          *                                  element in the set of matched elements.
