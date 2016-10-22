32ced7d3ac7d27a881240fe57372b890e52f1b43
Stefan Gabos <contact@stefangabos.ro>
2016-10-22 23:52:29 +0300

    Added the "insertAfter" method and minor documentation updates to "append", "appendTo" and "after" methods

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -163,7 +163,7 @@ $ = function(selector, parent, first_only) {
          *
          *  Both this and the {@link $#insertAfter .insertAfter()} method perform the same task, the main difference being in the
          *  placement of the content and the target. With `.after()`, the selector expression preceding the method is the target
-         *  after which the content is to be inserted. On the other hand, with `.insertInto()`, the content precedes the method,
+         *  after which the content is to be inserted. On the other hand, with `.insertAfter()`, the content precedes the method,
          *  and it is the one inserted after the target container.
          *
          *  > If there is more than one target element, clones of the inserted element will be created after each target except
@@ -189,9 +189,6 @@ $ = function(selector, parent, first_only) {
          *  // if "parent" is a collection of elements, clones of the list element will be created after
          *  // each target, except for the last one; the original list will be moved after the last target
          *  target.after($('ul'));
-         *
-         *  // insert a text node we create on the fly
-         *  target.append(document.createTextNode('hello'));
          *
          *  // insert a string (which will be transformed in HTML)
          *  // this is more efficient memory wise
@@ -278,9 +275,6 @@ $ = function(selector, parent, first_only) {
          *  // each target except for the last one; for the last target, the original list will be moved
          *  parent.append($('ul'));
          *
-         *  // append a text node we create on the fly
-         *  parent.append(document.createTextNode('hello'));
-         *
          *  // append a string (which will be transformed in HTML)
          *  // this is more efficient memory wise
          *  parent.append('<div>hello</div>');
@@ -328,7 +322,7 @@ $ = function(selector, parent, first_only) {
         }
 
         /**
-         *  Inserts every element in the set of matched elements to the end of the parent element, specified by the argument.
+         *  Inserts every element in the set of matched elements to the end of the parent element(s), specified by the argument.
          *
          *  Both this and the {@link $#append .append()} method perform the same task, the main difference being in the placement
          *  of the content and the target. With `.append()`, the selector expression preceding the method is the container into
@@ -359,12 +353,9 @@ $ = function(selector, parent, first_only) {
          *  // each target except for the last one; for the last target, the original list will be moved
          *  $('ul').appendTo(parent);
          *
-         *  // append a text node we create on the fly
-         *  $(document.createTextNode('hello')).appendTo(parent);
+         *  @param  {$}     parent      A ZebraJS object at end of which to insert each element in the set of matched elements.
          *
-         *  @param  {mixed}     parent      A ZebraJS object at end of which to insert each element in the set of matched elements.
-         *
-         *  @return {$}         Returns the ZebraJS object you are appending to.
+         *  @return {$}     Returns the ZebraJS object you are appending to.
          */
         this.appendTo = function(parent) {
 
@@ -771,9 +762,45 @@ $ = function(selector, parent, first_only) {
         }
 
         /**
-         *  @todo   Needs to be written!
+         *  Inserts every element in the set of matched elements after the parent element(s), specified by the argument.
+         *
+         *  Both this and the {@link $#after .after()} method perform the same task, the main difference being in the
+         *  placement of the content and the target. With `.after()`, the selector expression preceding the method is the target
+         *  after which the content is to be inserted. On the other hand, with `.insertAfter()`, the content precedes the method,
+         *  and it is the one inserted after the target container.
+         *
+         *  > If there is more than one target element, clones of the inserted element will be created after each target except
+         *  for the last one. The original item will be inserted after the last target.
+         *
+         *  > If an element selected this way is inserted elsewhere in the DOM, clones of the inserted element will be created
+         *  after each target except for the last one. The original item will be moved (not cloned) after the last target.
+         *
+         *  @example
+         *
+         *  // always cache selectors
+         *  // to avoid DOM scanning over and over again
+         *  var target = $('#selector');
+         *
+         *  // insert a div that we create on the fly
+         *  $('<div>').text('hello').insertAfter(target);
+         *
+         *  // same thing as above
+         *  $('<div>hello</div>').insertAfter(target);
+         *
+         *  // use one or more elements that already exist on the page
+         *  // if "target" is a single element than the list will be moved after the target element
+         *  // if "parent" is a collection of elements, clones of the list element will be created after
+         *  // each target, except for the last one; the original list will be moved after the last target
+         *  $('ul').insertAfter(target);
+         *
+         *  @param  {$}     target  A ZebraJS object after which to insert each element in the set of matched elements.
+         *
+         *  @return {$}     Returns the ZebraJS object you are inserting your elements after.
          */
-        this.insertAfter = function() {
+        this.insertAfter = function(target) {
+
+            // reverse the arguments and call the "append" method
+            return $(target).after(this);
 
         }
 
