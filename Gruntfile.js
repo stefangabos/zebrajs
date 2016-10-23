f717ac4ae3a25ae2bc6e3af80f007d8d410e6e7c
Stefan Gabos <contact@stefangabos.ro>
2016-10-23 21:55:58 +0300

    Include private methods in the documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,7 +129,8 @@ module.exports = function(grunt) {
                     destination: 'docs',
                     template : "node_modules/docdash",
                     configure : "jsdoc.json",
-                    readme: "Readme.md"
+                    readme: "Readme.md",
+                    private: true
                 }
             }
         },
