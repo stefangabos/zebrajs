5ad221df7eec618bdb91dc328dda5ff1fcaa7a45
Stefan Gabos <contact@stefangabos.ro>
2016-10-25 21:53:06 +0300

    Added the "insertBefore" method

--- src/insertBefore.js
+++ src/insertBefore.js
@@ -1,6 +1,42 @@
 /**
- *  @todo   Needs to be written!
+ *  Inserts every element in the set of matched elements before the parent element(s), specified by the argument.
+ *
+ *  Both this and the {@link $#before .before()} method perform the same task, the main difference being in the
+ *  placement of the content and the target. With `.before()`, the selector expression preceding the method is the target
+ *  before which the content is to be inserted. On the other hand, with `.insertBefore()`, the content precedes the method,
+ *  and it is the one inserted before the target element(s).
+ *
+ *  > If there is more than one target element, clones of the inserted element will be created before each target except
+ *  for the last one. The original item will be inserted before the last target.
+ *
+ *  > If an element selected this way is inserted elsewhere in the DOM, clones of the inserted element will be created
+ *  before each target except for the last one. The original item will be moved (not cloned) before the last target.
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var target = $('#selector');
+ *
+ *  // insert a div that we create on the fly
+ *  $('<div>').text('hello').insertBefore(target);
+ *
+ *  // same thing as above
+ *  $('<div>hello</div>').insertBefore(target);
+ *
+ *  // use one or more elements that already exist on the page
+ *  // if "target" is a single element than the list will be moved before the target element
+ *  // if "parent" is a collection of elements, clones of the list element will be created before
+ *  // each target, except for the last one; the original list will be moved before the last target
+ *  $('ul').insertBefore(target);
+ *
+ *  @param  {$}     target  A ZebraJS object before which to insert each element in the set of matched elements.
+ *
+ *  @return {$}     Returns the ZebraJS object before which the content is inserted.
  */
-this.insertBefore = function() {
+this.insertBefore = function(target) {
+
+    // call the "_dom_insert" private method with these arguments
+    return $(target)._dom_insert(this, 'before');
 
 }
