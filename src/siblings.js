6b2e23ae061f76316655922b434bb882cb692c2c
Stefan Gabos <contact@stefangabos.ro>
2016-10-28 23:43:05 +0300

    Added the "siblings" method

--- src/siblings.js
+++ src/siblings.js
@@ -3,4 +3,22 @@
  */
 this.siblings = function() {
 
+    var result = [];
+
+    // iterate through the set of matched elements
+    elements.forEach(function(element) {
+
+        // get the element's parent's children nodes that are not the current element, and add them to the results array
+        result = result.concat(Array.prototype.filter.call(element.parentNode.children, function(child) {
+
+            // skip the current element
+            return child !== element;
+
+        }));
+
+    });
+
+    // return the result, as a ZebraJS object
+    return $(result);
+
 }
