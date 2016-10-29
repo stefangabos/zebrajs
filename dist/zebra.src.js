3d8bf53c2a4117f6f9b7222f870bff1c2d1f638d
Stefan Gabos <contact@stefangabos.ro>
2016-10-29 19:43:31 +0300

    Added the finalized "siblings" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -1287,17 +1287,39 @@ $ = function(selector, parent, first_only) {
         }
 
         /**
-         *  @todo   Needs to be written!
+         *  Gets the siblings of each element in the set of matched elements, optionally filtered by a selector.
+         *
+         *  @example
+         *
+         *  // always cache selectors
+         *  // to avoid DOM scanning over and over again
+         *  var element = $('#selector');
+         *
+         *  // get all the siblings of the element
+         *  var siblings_all = element.siblings();
+         *
+         *  // get all the "div" siblings of the element
+         *  var siblings_filtered = element.siblings('div');
+         *
+         *  // since this method returns a ZebraJS object, we can use chaining
+         *  element.siblings('div').addClass('someclass');
+         *
+         *  @param  {string}    selector    If the selector is supplied, the elements will be filtered by testing whether they
+         *                                  match it.
+         *
+         *  @return {$}         Returns the siblings of each element in the set of matched elements, as a ZebraJS object, so you
+         *                      can use chaining.
          */
-        this.siblings = function() {
+        this.siblings = function(selector) {
 
             var result = [];
 
             // iterate through the set of matched elements
             elements.forEach(function(element) {
 
-                // get the element's parent's children nodes that are not the current element, and add them to the results array
-                result = result.concat(Array.prototype.filter.call(element.parentNode.children, function(child) {
+                // get the element's parent's children nodes which, optionally, match a given selector
+                // and add them to the results array
+                result = result.concat(Array.prototype.filter.call(selector ? element.parentNode.querySelectorAll(selector) : element.parentNode.children, function(child) {
 
                     // skip the current element
                     return child !== element;
