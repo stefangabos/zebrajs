a68b7d80561acb062af809af5d3292a78bbcde77
Stefan Gabos <contact@stefangabos.ro>
2016-11-02 18:40:40 +0200

    The method now relies on the "_dom_search" private method

--- src/children.js
+++ src/children.js
@@ -24,34 +24,7 @@
  */
 this.children = function(selector) {
 
-    var result = [], remove_id;
-
-    // iterate through the set of matched elements
-    elements.forEach(function(element) {
-
-        remove_id = false;
-
-        // if selector is specified and element does not have an ID
-        if (selector && null === element.getAttribute('id')) {
-
-            // generate and set a random ID for the element
-            element.setAttribute('id', $this._random('id'));
-
-            // set this flag so that we know to remove the randomly generated ID when we're done
-            remove_id = true;
-
-        }
-
-        // get the element's children nodes which, optionally, match a given selector
-        // and add them to the results array
-        result = result.concat(Array.prototype.slice.call(selector ? element.parentNode.querySelectorAll('#' + element.id + '>' + selector) : element.children));
-
-        // if present, remove the randomly generated ID
-        if (remove_id) element.removeAttribute('id');
-
-    });
-
-    // return the result, as a ZebraJS object
-    return $(result);
+    // get the children of each element in the set of matched elements, optionally filtered by a selector
+    return this._dom_search('siblings', selector);
 
 }
