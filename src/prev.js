d2240a32a548c8e7da29ad924360a7d99f1fead7
Stefan Gabos <contact@stefangabos.ro>
2016-11-04 08:56:52 +0200

    Added the "prev" method

--- src/prev.js
+++ src/prev.js
@@ -1,6 +1,32 @@
 /**
- *  @todo   Needs to be written!
+ *  Gets the immediately preceding sibling of each element in the set of matched elements. If a selector is provided,
+ *  it retrieves the previous sibling only if it matches that selector.
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var element = $('#selector');
+ *
+ *  // get the previous element
+ *  var prev = element.prev();
+ *
+ *  // get the previous element only if it matches the selector
+ *  var prev = element.prev('div');
+ *
+ *  // since this method returns a ZebraJS object, we can use chaining
+ *  element.prev().addClass('someclass');
+ *
+ *  @param  {string}    selector    If the selector is provided, the method will retrieve the previous sibling only if
+ *                                  it matches the selector
+ *
+ *  @return {$}         Returns the immediately preceding sibling of each element in the set of matched elements,
+ *                      optionally filtered by a selector, as a ZebraJS object, so you can use chaining.
  */
-this.prev = function() {
+this.prev = function(selector) {
+
+    // get the immediately preceding sibling of each element in the set of matched elements,
+    // optionally filtered by a selector
+    return this._dom_search('previous', selector);
 
 }
