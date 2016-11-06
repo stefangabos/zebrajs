1e173c5dcc8d2f6bdcec647399b0bff56f4aa8e6
Stefan Gabos <contact@stefangabos.ro>
2016-11-06 22:37:45 +0200

    The method now works like the one from jQuery

--- src/each.js
+++ src/each.js
@@ -3,11 +3,7 @@
  *
  *  @param  {function}  callback    The function to execute for each item in the set. The callback function
  *                                  receives as single argument the element's position in the set, called `index`
- *                                  (0-based). The `this` keyword inside the callback function refers to the
- *                                  wrapped element (in a "$" object) the callback function is currently applied
- *                                  to.
- *                                  <br><br>
- *                                  *Use {@link $#_dom _dom()} to refer to the DOM node instead.*
+ *                                  (0-based). The `this` keyword inside the callback function refers to the DOM element.
  *                                  <br><br>
  *                                  *Returning `FALSE` from the callback function breaks the loop!*
  *
@@ -18,14 +14,14 @@
  *      // show the element's index in the set
  *      console.log(index);
  *
- *      // remember, the "this" keyword refers
- *      // to the wrapped element (in a "$" object)
- *      this.css('display', 'none');
- *
- *      // the original DOM node
- *      console.log(this._dom());
+ *      // remember, inside the callback, the "this" keyword refers to the DOM element
+ *      // so we'll have to use $() around it to transform it into a ZebraJS object
+ *      // (as always, cache it if you need to use it more than once)
+ *      $(this).css('display', 'none');
  *
  *  });
+ *
+ *  @return {undefined}
  */
 this.each = function(callback) {
 
@@ -35,6 +31,6 @@ this.each = function(callback) {
         //  apply the callback function (the index is the argument to the function, while the "this" keyword
         //  inside the callback function refers to wrapped element (in a "$" object)
         //  returning false from the callback function exists the loop
-        if (callback.call(new $(elements[i]), i) === false) return;
+        if (callback.call(elements[i], i) === false) return;
 
 }
