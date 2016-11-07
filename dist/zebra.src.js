8d7000673d88a274c944c9b580d4a5cc083cddcd
Stefan Gabos <contact@stefangabos.ro>
2016-11-07 20:26:08 +0200

    Added the "first" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -867,9 +867,23 @@ $ = function(selector, parent, first_only) {
         }
 
         /**
-         *  @todo   Needs to be written!
+         *  Constructs a new ZebraJS object from the first element in the set of matched elements.
+         *
+         *  @example
+         *
+         *  // always cache selectors
+         *  // to avoid DOM scanning over and over again
+         *  var elements = $('selector');
+         *
+         *  // returns the first element from the list of matched elements, as a ZebraJS object
+         *  var first = elements.first();
+         *
+         *  @return {$}         Returns the first element from the list of matched elements, as a ZebraJS object
          */
         this.first = function() {
+
+            // returns the first element from the list of matched elements, as a ZebraJS object
+            return $(elements[0]);
 
         }
 
--- src/first.js
+++ src/first.js
@@ -1,6 +1,20 @@
 /**
- *  @todo   Needs to be written!
+ *  Constructs a new ZebraJS object from the first element in the set of matched elements.
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var elements = $('selector');
+ *
+ *  // returns the first element from the list of matched elements, as a ZebraJS object
+ *  var first = elements.first();
+ *
+ *  @return {$}         Returns the first element from the list of matched elements, as a ZebraJS object
  */
 this.first = function() {
+
+    // returns the first element from the list of matched elements, as a ZebraJS object
+    return $(elements[0]);
 
 }
