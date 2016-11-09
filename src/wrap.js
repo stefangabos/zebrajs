b929fdad0958f141843bc000f4260f2ebd2b7b97
Stefan Gabos <contact@stefangabos.ro>
2016-11-09 21:35:38 +0200

    Fixed typo

--- src/wrap.js
+++ src/wrap.js
@@ -2,7 +2,7 @@
  *  Wraps an HTML structure around each element in the set of matched elements.
  *
  *  > Note that if the method's argument is a selector then clones of the element described by the selector will be
- *  created and wrapped around each element in the set o fmatched elements except for the last on. The original item will
+ *  created and wrapped around each element in the set of matched elements except for the last one. The original item will
  *  be moved (not cloned) and wrapped around the last target.
  *
  *  @example
