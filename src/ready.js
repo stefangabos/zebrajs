cae42bfb6b09b10f3a9ec97bf0903c84510fad3a
Stefan Gabos <contact@stefangabos.ro>
2016-11-14 12:36:36 +0200

    Added documentation

--- src/ready.js
+++ src/ready.js
@@ -1,10 +1,25 @@
 /**
- *  @todo   Needs documentation!
+ *  Specifies a function to execute when the DOM is fully loaded.
+ *
+ *  @example
+ *
+ *  $(document).ready(function() {
+ *      // code to be executed when the DOM is ready
+ *  });
+ *
+ *  @param  {function}  callback    A function to execute when the DOM is ready and safe to manipulate.
+ *
+ *  @return {$}         Returns the set of matched elements.
  */
 this.ready = function(callback) {
 
+    // if DOM is already ready, fire the callback now
     if (document.readyState === 'complete' || document.readyState !== 'loading') callback();
 
+    // otherwise, wait for the DOM and execute the callback when the it is ready
     else document.addEventListener('DOMContentLoaded', callback);
+
+    // return the set of matched elements
+    return $this;
 
 }
