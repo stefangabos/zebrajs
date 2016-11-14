4f96c762948726c5acb6dd52caee5157df5f4328
Stefan Gabos <contact@stefangabos.ro>
2016-11-14 19:49:01 +0200

    Added documentation for the "position" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -1588,10 +1588,28 @@
             }
 
             /**
-             *  @todo   Needs to be written!
+             *  Gets the current coordinates of the first element in the set of matched elements, relative to the offset parent.
+             *
+             *  This method retrieves the current position of an element relative to the offset parent, in contrast this with the
+             *  {@link $.$#offset .offset()} method which retrieves the current position relative to the document.
+             *
+             *  > This method cannot get the position of hidden elements or accounting for borders, margins, or padding set on the
+             *  body element.
+             *
+             *  @example
+             *
+             *  // always cache selectors
+             *  // to avoid DOM scanning over and over again
+             *  var element = $('#selector');
+             *
+             *  // get the element's position, relative to the offset parent
+             *  var coordinates = element.position()
+             *
+             *  @return {object}    Returns an object with the `left` and `top` properties.
              */
             this.position = function() {
 
+                // return the position of the first element in the set of matched elements
                 return {
                     left: parseFloat(elements[0].offsetLeft),
                     top: parseFloat(elements[0].offsetTop)
