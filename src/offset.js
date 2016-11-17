1bdd87e8a16aea28564cab921a0741f53995af93
Stefan Gabos <contact@stefangabos.ro>
2016-11-17 18:05:43 +0200

    Updated documentation

--- src/offset.js
+++ src/offset.js
@@ -1,13 +1,32 @@
 /**
- *  @todo   Needs to be written!
+ *  Gets the current coordinates of the first element in the set of matched elements, relative to the document.
+ *
+ *  This method retrieves the current position of an element relative to the document, in contrast with the
+ *  {@link $.$#position .position()} method which retrieves the current position relative to the offset parent.
+ *
+ *  > This method cannot get the position of hidden elements or accounting for borders, margins, or padding set on the
+ *  body element.
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var element = $('#selector');
+ *
+ *  // get the element's position, relative to the offset parent
+ *  var offset = element.offset()
+ *
+ *  @return {object}    Returns an object with the `left` and `top` properties.
  */
 this.offset = function() {
 
-    var i = elements[0].getBoundingClientRect();
+    // get the bounding box of the first element in the set of matched elements
+    var box = elements[0].getBoundingClientRect();
 
+    // return the object with the offset
     return {
-        top: i.top + window.pageYOffset - document.documentElement.clientTop,
-        left: i.left + window.pageXOffset - document.documentElement.clientLeft
+        left: box.left + window.pageXOffset - document.documentElement.clientLeft,
+        top: box.top + window.pageYOffset - document.documentElement.clientTop
     }
 
 }
--- src/position.js
+++ src/position.js
@@ -1,7 +1,7 @@
 /**
  *  Gets the current coordinates of the first element in the set of matched elements, relative to the offset parent.
  *
- *  This method retrieves the current position of an element relative to the offset parent, in contrast this with the
+ *  This method retrieves the current position of an element relative to the offset parent, in contrast with the
  *  {@link $.$#offset .offset()} method which retrieves the current position relative to the document.
  *
  *  > This method cannot get the position of hidden elements or accounting for borders, margins, or padding set on the
@@ -14,7 +14,7 @@
  *  var element = $('#selector');
  *
  *  // get the element's position, relative to the offset parent
- *  var coordinates = element.position()
+ *  var position = element.position()
  *
  *  @return {object}    Returns an object with the `left` and `top` properties.
  */
