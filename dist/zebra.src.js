578b048d3db7a9e01247c6c2362113d71be71fbc
Stefan Gabos <contact@stefangabos.ro>
2016-11-19 12:11:51 +0200

    Added the "scrollTop" method and added a shorthand for selecting the "body" element

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -33,6 +33,8 @@
         // if called without the "new" keyword
         if (!(this instanceof $)) {
 
+            if (selector === 'body') selector = document.body;
+
             // if selector is given as a string
             if (typeof selector === 'string') {
 
@@ -434,7 +436,7 @@
              *
              *  // chaining
              *  elements.addClass('foo baz').css('display', 'none');
-
+             *
              *  @param  {string}    class_name  One or more space-separated class names to be added to each element in the
              *                                  set of matched elements.
              *
@@ -2159,9 +2161,46 @@
             }
 
             /**
-             *  @todo   Needs to be written!
+             *  Gets the vertical position of the scrollbar for the first element in the set of matched elements, or sets the
+             *  vertical position of the scrollbar for every matched element.
+             *
+             *  The vertical scroll position is the same as the number of pixels that are hidden from view above the scrollable area.
+             *  If the scroll bar is at the very top, or if the element is not scrollable, this number will be `0`.
+             *
+             *  @example
+             *
+             *  // always cache selectors
+             *  // to avoid DOM scanning over and over again
+             *  var body = $('body');
+             *
+             *  // get the vertical scroll of the body
+             *  body.scrollTop();
+             *
+             *  // set the vertical scroll of the body
+             *  body.scrollTop(250);
+             *
+             *  // chaining
+             *  elements.scrollTop(250).addClass('foo');
+             *
+             *  @param  {integer}   [value]     Sets the vertical position of the scrollbar for every matched element.
+             *
+             *  @return {$|integer}             When `setting` the vertical position, this method returns the set of matched elements.
+             *                                  When `reading` the vertical position, this method returns the vertical position of
+             *                                  the scrollbar for the first element in the set of matched elements.
              */
-            this.scrollTop = function() {
+            this.scrollTop = function(value) {
+
+                // if value is not specified, return the scrollTop value of the first element in the set of matched elements
+                if (undefined === value) return elements[0].scrollTop;
+
+                // iterate through the set of matched elements
+                elements.forEach(function(element) {
+
+                    // set the scrollTop value for each element
+                    // apply "parseFloat" in case is provided as string or suffixed with "px"
+                    element.scrollTop = parseFloat(value);
+
+                });
 
             }
 
