abbf029673197d68e9c5eba536dc4d4ff32e2543
Stefan Gabos <contact@stefangabos.ro>
2016-11-19 14:07:35 +0200

    Added the "scrollLeft" method

--- src/scrollLeft.js
+++ src/scrollLeft.js
@@ -1,6 +1,43 @@
 /**
- *  @todo   Needs to be written!
+ *  Gets the horizontal position of the scrollbar for the first element in the set of matched elements, or sets the
+ *  horizontal position of the scrollbar for every matched element.
+ *
+ *  The horizontal scroll position is the same as the number of pixels that are hidden from view above the scrollable area.
+ *  If the scroll bar is at the very left, or if the element is not scrollable, this number will be `0`.
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var body = $('body');
+ *
+ *  // get the horizontal scroll of the body
+ *  body.scrollLeft();
+ *
+ *  // set the horizontal scroll of the body
+ *  body.scrollLeft(250);
+ *
+ *  // chaining
+ *  elements.scrollLeft(250).addClass('foo');
+ *
+ *  @param  {integer}   [value]     Sets the horizontal position of the scrollbar for every matched element.
+ *
+ *  @return {$|integer}             When `setting` the horizontal position, this method returns the set of matched elements.
+ *                                  When `reading` the horizontal position, this method returns the horizontal position of
+ *                                  the scrollbar for the first element in the set of matched elements.
  */
-this.scrollLeft = function() {
+this.scrollLeft = function(value) {
+
+    // if value is not specified, return the scrollLeft value of the first element in the set of matched elements
+    if (undefined === value) return elements[0].scrollLeft;
+
+    // iterate through the set of matched elements
+    elements.forEach(function(element) {
+
+        // set the scrollLeft value for each element
+        // apply "parseFloat" in case is provided as string or suffixed with "px"
+        element.scrollLeft = parseFloat(value);
+
+    });
 
 }
