6159613b8a6bc1f295243499c3a58e0d4167773b
Stefan Gabos <contact@stefangabos.ro>
2016-11-29 22:02:55 +0200

    Added documentation for the "get" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -1295,7 +1295,19 @@
     }
 
     /**
-     *  @todo   Needs to be documented!
+     *  Retrieves one of the elements matched by the {@link ZebraJS} object.
+     *
+     *  @example
+     *
+     *  // always cache selectors
+     *  // to avoid DOM scanning over and over again
+     *  var elements = $('selector');
+     *
+     *  // this gets the second DOM element from the list of matched elements
+     *  elements.get(1);
+     *
+     *  @param  {integer}   index   The index (starting from `0`) of the DOM element to return from the list of matched
+     *                              elements
      *
      *  @memberof   ZebraJS
      *  @alias      get
@@ -1303,6 +1315,7 @@
      */
     $.fn.get = function(index) {
 
+        // return the matching DOM element
         return this[index];
 
     }
