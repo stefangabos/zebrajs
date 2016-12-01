af6bb5a00d1db8da60b0141a079624622f67650d
Stefan Gabos <contact@stefangabos.ro>
2016-12-01 23:10:44 +0200

    Fixed bugs with "one" and "off" methods and added the "one" method

--- dist/zebra.src.js
+++ dist/zebra.src.js
@@ -1239,7 +1239,7 @@
             if (typeof selector === 'object' && selector.version)
 
                 // iterate through the elements in the object
-                selector.each(function(index, wrapped) {
+                selector.forEach(function(wrapped) {
 
                     // if the elements are the same, add it to the results array
                     if (wrapped.isSameNode(element)) result.push(element);
@@ -1669,7 +1669,7 @@
         this.forEach(function(element) {
 
             // iterate through the event types we have to remove the handler from
-            event_types.each(function(index, event_type) {
+            event_types.forEach(function(event_type) {
 
                 // handle namespacing
                 namespace = event_type.split('.')
@@ -1781,7 +1781,7 @@
      *      console.log('clicked!');
      *  });
      *
-     *  // namespacing, so that you can only remove certain events
+     *  // namespacing, so that you can remove only certain events
      *  element.on('click.namespace', function(e) {
      *      console.log('clicked!');
      *  });
@@ -1813,7 +1813,7 @@
      *  @alias      on
      *  @instance
      */
-    $.fn.on = function(event_type, selector, callback) {
+    $.fn.on = function(event_type, selector, callback, once) {
 
         var event_types = event_type.split(' '), namespace, actual_callback;
 
@@ -1825,12 +1825,12 @@
         this.forEach(function(element) {
 
             // iterate through the event types we have to attach the handler to
-            event_types.forEach(function(event_type) {
+            event_types.forEach(function(original_event) {
 
                 actual_callback = false;
 
                 // handle namespacing
-                namespace = event_type.split('.')
+                namespace = original_event.split('.')
                 event_type = namespace[0];
                 namespace = namespace[1] || '';
 
@@ -1846,7 +1846,10 @@
                     // this will be the actual callback function
                     actual_callback = function(e) {
 
-                        // trigger the callback function only if the clicked element matches the selector
+                        // if the callback needs to be executed only once, remove it now
+                        if (once) $(this).off(original_event, callback);
+
+                        // trigger the callback function only if the target element matches the selector
                         if (this !== e.target && e.target.matches(selector)) callback.apply(e.target);
 
                     };
@@ -1854,7 +1857,24 @@
                     // attach event listener
                     element.addEventListener(event_type, actual_callback);
 
-                // set the event listener
+                // if the callback needs to be executed only once
+                } else if (once) {
+
+                    // the actual callback function
+                    actual_callback = function(e) {
+
+                        // remove the event handler
+                        $(this).off(original_event, callback);
+
+                        // execute the callback function
+                        callback();
+
+                    }
+
+                    // set the event listener
+                    element.addEventListener(event_type, actual_callback);
+
+                // registering of default event listeners
                 } else element.addEventListener(event_type, callback);
 
                 // add element/callback combination to the array of events of this type
@@ -1866,6 +1886,72 @@
 
         // return the set of matched elements, for chaining
         return this;
+
+    }
+
+    /**
+     *  Attaches an event handler function for one or more events to the selected elements. The event handler is executed at
+     *  most once per element per event type.
+     *
+     *  @example
+     *
+     *  // always cache selectors
+     *  // to avoid DOM scanning over and over again
+     *  var element = $('#selector');
+     *
+     *  // create a function
+     *  var callback = function(e) {
+     *      console.log('clicked!');
+     *  }
+     *
+     *  // handle clicks on element using the function created above
+     *  // (the callback will be executed only once)
+     *  element.one('click', callback);
+     *
+     *  // handle clicks on element using an anonymous function
+     *  // (the callback will be executed only once)
+     *  element.one('click', function(e) {
+     *      console.log('clicked!');
+     *  });
+     *
+     *  // namespacing, so that you can remove only certain events
+     *  // (the callback will be executed only once)
+     *  element.one('click.namespace', function(e) {
+     *      console.log('clicked!');
+     *  });
+     *
+     *  // using delegation
+     *  // handle clicks on all the "div" elements
+     *  // that are children of the element
+     *  // (the callback will be executed only once for each matched element)
+     *  element.one('click', 'div', function(e) {
+     *      console.log('clicked!');
+     *  });
+     *
+     *  // chaining
+     *  element.one('click', function() {
+     *      console.log('clicked!');
+     *  }).addClass('foo');
+     *
+     *  @param  {string}    event_type  One or more space-separated event types and optional namespaces, such as "click" or
+     *                                  "click.namespace".
+     *
+     *  @param  {string}    [selector]  A selector string to filter the descendants of the selected elements that will call
+     *                                  the handler. If the selector is null or omitted, the handler is always called when it
+     *                                  reaches the selected element.
+     *
+     *  @param  {function}  callback    A function to execute when the event is triggered.
+     *
+     *  @return {ZebraJS}   Returns the set of matched elements.
+     *
+     *  @memberof   ZebraJS
+     *  @alias      one
+     *  @instance
+     */
+    $.fn.one = function(event_type, selector, callback) {
+
+        // call the "on" method with last argument set to TRUE
+        this.on(event_type, selector, callback, true);
 
     }
 
