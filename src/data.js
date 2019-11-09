9a6c649bc21dc615fd686cac4eca79a115dcfd33
Alphons van der Heijden <alphons@heijden.com>
2019-11-09 16:19:50 +0100

    Only first data change was applied, so of nu use. Now dataset is targetted directly, no overhead. works as axpected.

--- src/data.js
+++ src/data.js
@@ -22,91 +22,45 @@
  *  @param  {mixed}     value       The value to associate with the data set.
  *
  *  @return {ZebraJS|mixed}         When `setting` data attributes, this method returns the set of matched elements.
- *                                  When `reading` data attributes, this method returns the stored values, or `undefined`
+ *                                  When `reading` data attributes, this method returns the stored value, or `undefined`
  *                                  if not data found for the requested key.
  *
  *  @memberof   ZebraJS
  *  @alias      data
  *  @instance
  */
-$.fn.data = function(name, value) {
+    $.fn.data = function(name, value) {
 
-    // if no name is given, return "undefined"
-    if (undefined === name) return undefined;
+        // if no name is given, return "undefined"
+        if (undefined === name) return undefined;
 
-    // make sure the name follows the Dataset API specs
-    // http://www.w3.org/TR/html5/dom.html#dom-dataset
-    name = name
+        // make sure the name follows the Dataset API specs
+        // http://www.w3.org/TR/html5/dom.html#dom-dataset
+        name = name
 
-        // replace "-" followed by an ascii letter to that letter in uppercase
-        .replace(/\-([a-z])/ig, function() { return arguments[1].toUpperCase(); })
+            // replace "-" followed by an ascii letter to that letter in uppercase
+            .replace(/\-([a-z])/ig, function() { return arguments[1].toUpperCase(); })
 
-        // remove any left "-"
-        .replace(/\-/g, '');
+            // remove any left "-"
+            .replace(/\-/g, '');
 
-    // iterate through the set of matched elements
-    this.some(function(element) {
-
-        // initialize the "zjs" "private" property, if not already initialized
-        if (!element.zjs) element.zjs = {};
-
-        // if not already initialized...
-        if (!element.zjs.data) {
-
-            // ...initialize the "data" property,
-            element.zjs.data = {};
-
-            // iterate over the element's existing attributes
-            Array.prototype.slice.call(element.attributes).forEach(function(attribute) {
-
-                // if we found an attribute starting with "data-"
-                if (attribute.name.indexOf('data-') === 0) {
-
-                    // make sure the name follows the Dataset API specs
-                    var name = attribute.name
-
-                        // remove the "data-" prefix
-                        .replace(/^data\-/, '')
-
-                        // replace "-" followed by an ascii letter to that letter in uppercase
-                        .replace(/\-([a-z])/ig, function() { return arguments[1].toUpperCase(); })
-
-                        // remove any left "-"
-                        .replace(/\-/g, '');
-
-                    // store the data attribute
-                    element.zjs.data[name] = attribute.value;
-
-                }
-
-            });
-
-        }
-
-        // if "value" argument is not provided
-        // break out of "some" after the first element
-        if (undefined === value) return true;
-
-        // if value is not already set, or it is set but it is of different type, set it now
-        if (!element.zjs.data[name] || typeof value !== typeof element.zjs.data[name]) element.zjs.data[name] = value;
-
-        // if both the existing value and the new one are objects
-        else if (typeof value === 'object' && typeof element.zjs.data[name] === 'object')
-
-            // merge the new values with the old ones
-            Object.keys(value).forEach(function(key) {
-
-                element.zjs.data[name][key] = value[key];
-
-            });
-
-    });
-
-    // if "value" argument is provided
-    // return the set of matched elements
-    if (undefined !== value) return this;
-
-    // if "value" argument is not provided, return the existing value, or "undefined" if no value exists
-    return this[0].zjs ? this[0].zjs.data[name] : undefined;
-
-}
+		// setting dataset value
+		if(value !== undefined) {
+			// iterate through the set of matched elements
+			this.forEach(function(element) {
+				element.dataset[name] = value;
+			});
+			// return collection
+			return this;
+		}
+		
+		// gettings first value, if exist
+		this.some(function(element) {
+			if(element.dataset[name] !== undefined) {
+				value = element.dataset[name];
+				return true;
+			}
+		});
+		
+		return value;
+    }
