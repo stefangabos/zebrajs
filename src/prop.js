a5257f70f48d20eb7e1e536445ab9a8dd69c0202
stefangabos <contact@stefangabos.ro>
2020-03-21 11:51:49 +0200

    Source code formatting

--- src/prop.js
+++ src/prop.js
@@ -1,49 +1,54 @@
 /**
- *  @todo   Needs to be written!
+ *  Gets the value of a property for the first element in the set of matched elements, or set one or more properties for
+ *  every matched element.
  */
-$.fn.prop = function (prop, value)
-{
-	// if property argument is an object
-	if (typeof prop === 'object')
-		// iterate over the set of matched elements
-		this.forEach(function (element)
-		{
-			// iterate over the properties
-			for (var i in prop)
-				// set each property
-				element[i] = prop[i];
-		});
-	// if property argument is a string
-	else if (typeof prop === 'string')
+$.fn.prop = function(prop, value) {
 
-		// if the value argument is provided
-		if (undefined !== value)
+    // if property argument is an object
+    if (typeof prop === 'object')
 
-			// iterate over the set of matched elements
-			this.forEach(function (element)
-			{
-				// if value argument's value is FALSE or NULL
-				if (value === false || value === null)
-				{
-					// remove the property (delete has problems in some browsers)
-					if (prop in element)
-						element[prop] = undefined;
-				}
-				// for other values, set the property value
-				else element[prop] = value;
+        // iterate over the set of matched elements
+        this.forEach(function(element) {
 
-			});
+            // iterate over the properties
+            for (var i in prop)
 
-		// if the value argument is not provided
-		else
-			// return the value of the requested property
-			// of the first element in the set of matched elements
-			if (prop in this[0])
-				return this[0][prop];
-			else
-				return undefined;
+                // set each property
+                element[i] = prop[i];
 
-	// if we get this far, return the set of matched elements
-	return this;
+        });
+
+    // if property argument is a string
+    else if (typeof prop === 'string')
+
+        // if the value argument is provided
+        if (undefined !== value)
+
+            // iterate over the set of matched elements
+            this.forEach(function(element) {
+
+                // if value argument's value is FALSE or NULL
+                if (value === false || value === null) {
+
+                    // remove the property (delete has problems in some browsers)
+                    if (prop in element) element[prop] = undefined;
+
+                // for other values, set the property value
+                } else element[prop] = value;
+
+            });
+
+        // if the value argument is not provided
+        // return the value of the requested property
+        // of the first element in the set of matched elements
+        else {
+
+            if (prop in this[0]) return this[0][prop];
+            else return undefined;
+
+        }
+
+    // if we get this far, return the set of matched elements
+    return this;
 
 };
