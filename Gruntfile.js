b1965ea071d8adccf949d1076238ec93052c28e2
stefangabos <contact@stefangabos.ro>
2022-01-15 10:13:21 +0200

    Updated packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
          **************************************************************************************************************/
         'eslint' : {
             options: {
-                configFile: 'eslint.json'
+                overrideConfigFile: 'eslint.json'
             },
             library: {
                 src: ['src/**/*.js']
