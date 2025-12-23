9af348e1154797114c832e7d8dac6c21cbf89525
Stefan Gabos <contact@stefangabos.ro>
2025-12-23 13:57:01 +0200

    Fixed bug and updated documentation

--- src/prop.js
+++ src/prop.js
@@ -1,6 +1,55 @@
 /**
- *  Gets the value of a property for the first element in the set of matched elements, or set one or more properties for
+ *  Gets the value of a property for the first element in the set of matched elements, or sets one or more properties for
  *  every matched element.
+ *
+ *  @example
+ *
+ *  // always cache selectors
+ *  // to avoid DOM scanning over and over again
+ *  var elements = $('selector');
+ *
+ *  // get the value of a property
+ *  // for the first element in the set of matched elements
+ *  elements.prop('disabled');
+ *
+ *  // set a single property
+ *  elements.prop('disabled', true);
+ *
+ *  // set multiple properties
+ *  elements.prop({
+ *      disabled: true,
+ *      checked: false
+ *  });
+ *
+ *  // remove a property
+ *  elements.prop('disabled', false);
+ *
+ *  // chaining
+ *  elements.prop('disabled', true).addClass('foo');
+ *
+ *  @param  {string|object} prop        If given as a `string` representing a property name and `value` **is not** set,
+ *                                      this method will return the value of that particular property for the first element
+ *                                      in the set of matched elements.
+ *                                      <br><br>
+ *                                      If given as a `string` representing a property name and `value` **is** set, this
+ *                                      method will set that particular property's value for all the elements in the set
+ *                                      of matched elements.
+ *                                      <br><br>
+ *                                      If given as an `object`, this method will set the given properties to the given
+ *                                      values for all the elements in the set of matched elements.
+ *
+ *  @param  {mixed}         [value]     The value to be set for the property given as argument. *Only used if `prop` is
+ *                                      not an object!*
+ *                                      <br><br>
+ *                                      Setting it to `false` or `null` will instead **delete** the property from the set
+ *                                      of matched elements.
+ *
+ *  @return {ZebraJS|mixed}             When `setting` properties, this method returns the set of matched elements. When
+ *                                      `reading` properties, this method returns the value of the requested property.
+ *
+ *  @memberof   ZebraJS
+ *  @alias      prop
+ *  @instance
  */
 $.fn.prop = function(prop, value) {
 
@@ -30,8 +79,8 @@ $.fn.prop = function(prop, value) {
                 // if value argument's value is FALSE or NULL
                 if (value === false || value === null) {
 
-                    // remove the property (delete has problems in some browsers)
-                    if (prop in element) element[prop] = undefined;
+                    // delete the property
+                    if (prop in element) delete element[prop];
 
                 // for other values, set the property value
                 } else element[prop] = value;
