6ab3e643e69137bc0107eb772a124d995ad0e5b5
Stefan Gabos <contact@stefangabos.ro>
2025-12-23 22:32:35 +0200

    Use ES6+ syntax and implemented suport for pseudo-selectors

--- src/$.js
+++ src/$.js
@@ -1,188 +1,197 @@
+/* global _process_pseudo_selector */
 (function() {
 
     'use strict';
 
-    var
+    // we'll use this to keep track of registered event listeners
+    // eslint-disable-next-line no-unused-vars
+    let event_listeners = {};
 
-        // we'll use this to keep track of registered event listeners
-        // eslint-disable-next-line no-unused-vars
-        event_listeners = {},
+    // we'll use this when generating random IDs
+    // eslint-disable-next-line no-unused-vars
+    let internal_counter = 0;
 
-        // we'll use this when generating random IDs
-        // eslint-disable-next-line no-unused-vars
-        internal_counter = 0,
+    // this is the function used internally to create ZebraJS objects using the given arguments
+    // at the end of this file we give it a simpler name, like "$", but internally we'll use it like it is
 
-        // this is the function used internally to create ZebraJS objects using the given arguments
-        // at the end of this file we give it a simpler name, like "$", but internally we'll use it like it is
+    /**
+    *   Creates a ZebraJS object which provides methods meant for simplifying the interaction with the set of
+    *   elements matched by the `selector` argument. This is referred to as `wrapping` those elements.
+    *
+    *   @example
+    *
+    *   // select an element by ID
+    *   var element = $('#foo');
+    *
+    *   // select element by class name
+    *   var elements = $('.foo');
+    *
+    *   // select elements by using JavaScript
+    *   var elements = $(document.querySelectorAll('.foo'));
+    *
+    *   // use CSS selectors
+    *   var elements = $('input[type=text]');
+    *
+    *   // create elements
+    *   var element = $('<div>').addClass('foo').appendTo($('body'));
+    *
+    *   // SECURITY WARNING: When creating HTML elements from strings, NEVER use untrusted user input directly.
+    *   // This could lead to Cross-Site Scripting (XSS) attacks.
+    *
+    *   // UNSAFE - user input could contain malicious code:
+    *   // var userInput = '<img src=x onerror=alert(1)>';
+    *   // $(userInput);  // XSS vulnerability!
+    *
+    *   // SAFE - create elements programmatically and set user data via methods:
+    *   // $('<div>').text(userInput);  // User input is safely escaped
+    *   // $('<img>').attr('src', userInput);  // Safely set attributes
+    *
+    *   // SAFE - sanitize user input with a library like DOMPurify before creating HTML:
+    *   // var sanitized = DOMPurify.sanitize(userInput);
+    *   // $(sanitized);
+    *
+    *   @param  {mixed}     selector        A selector to filter DOM elements from the current document. It can be a
+    *                                       query selector, a {@link ZebraJS} object, a DOM element, a
+    *                                       {@link https://developer.mozilla.org/en-US/docs/Web/API/NodeList NodeList},
+    *                                       and array of DOM elements<br><br>Alternatively, it can be a HTML tag
+    *                                       to create.<br>
+    *                                       <blockquote>**Never pass unsanitized user input when creating HTML elements,
+    *                                       as this can lead to XSS vulnerabilities!**</blockquote>
+    *
+    *   @param  {mixed}     [parent]        A selector to filter DOM elements from the current document, but only
+    *                                       those which have as parent the element(s) indicated by this argument. It
+    *                                       can be a query selector, a {@link ZebraJS} object, a DOM element, a
+    *                                       {@link https://developer.mozilla.org/en-US/docs/Web/API/NodeList NodeList},
+    *                                       and array of DOM elements
+    *
+    *   @param  {boolean}   [first_only]    Setting this argument will instruct the method to return only the first
+    *                                       element from the set of matched elements.
+    *
+    *   @return {array}     Returns a special array holding the matching elements and having all the methods to help
+    *                       you work with those elements.
+    *
+    *   @author     Stefan Gabos <contact@stefangabos.ro>
+    *   @version    2.0.0 (last revision December 23, 2025)
+    *   @copyright  (c) 2016-2025 Stefan Gabos
+    *   @license    LGPL-3.0
+    *   @alias      ZebraJS
+    *   @class
+    */
+    $ = function(selector, parent, first_only) {
 
-        /**
-        *   Creates a ZebraJS object which provides methods meant for simplifying the interaction with the set of
-        *   elements matched by the `selector` argument. This is referred to as `wrapping` those elements.
-        *
-        *   @example
-        *
-        *   // select an element by ID
-        *   var element = $('#foo');
-        *
-        *   // select element by class name
-        *   var elements = $('.foo');
-        *
-        *   // select elements by using JavaScript
-        *   var elements = $(document.querySelectorAll('.foo'));
-        *
-        *   // use CSS selectors
-        *   var elements = $('input[type=text]');
-        *
-        *   // create elements
-        *   var element = $('<div>').addClass('foo').appendTo($('body'));
-        *
-        *   // SECURITY WARNING: When creating HTML elements from strings, NEVER use untrusted user input directly.
-        *   // This could lead to Cross-Site Scripting (XSS) attacks.
-        *
-        *   // UNSAFE - user input could contain malicious code:
-        *   // var userInput = '<img src=x onerror=alert(1)>';
-        *   // $(userInput);  // XSS vulnerability!
-        *
-        *   // SAFE - create elements programmatically and set user data via methods:
-        *   // $('<div>').text(userInput);  // User input is safely escaped
-        *   // $('<img>').attr('src', userInput);  // Safely set attributes
-        *
-        *   // SAFE - sanitize user input with a library like DOMPurify before creating HTML:
-        *   // var sanitized = DOMPurify.sanitize(userInput);
-        *   // $(sanitized);
-        *
-        *   @param  {mixed}     selector        A selector to filter DOM elements from the current document. It can be a
-        *                                       query selector, a {@link ZebraJS} object, a DOM element, a
-        *                                       {@link https://developer.mozilla.org/en-US/docs/Web/API/NodeList NodeList},
-        *                                       and array of DOM elements<br><br>Alternatively, it can be a HTML tag
-        *                                       to create.<br>
-        *                                       <blockquote>**Never pass unsanitized user input when creating HTML elements,
-        *                                       as this can lead to XSS vulnerabilities!**</blockquote>
-        *
-        *   @param  {mixed}     [parent]        A selector to filter DOM elements from the current document, but only
-        *                                       those which have as parent the element(s) indicated by this argument. It
-        *                                       can be a query selector, a {@link ZebraJS} object, a DOM element, a
-        *                                       {@link https://developer.mozilla.org/en-US/docs/Web/API/NodeList NodeList},
-        *                                       and array of DOM elements
-        *
-        *   @param  {boolean}   [first_only]    Setting this argument will instruct the method to return only the first
-        *                                       element from the set of matched elements.
-        *
-        *   @return {array}     Returns a special array holding the matching elements and having all the methods to help
-        *                       you work with those elements.
-        *
-        *   @author     Stefan Gabos <contact@stefangabos.ro>
-        *   @version    2.0.0 (last revision December 23, 2025)
-        *   @copyright  (c) 2016-2025 Stefan Gabos
-        *   @license    LGPL-3.0
-        *   @alias      ZebraJS
-        *   @class
-        */
-        $ = function(selector, parent, first_only) {
+        let elements = [], result;
 
-            var elements = [], property;
+        // refer to document.body node if it is the case
+        if (typeof selector === 'string' && selector.toLowerCase() === 'body') selector = document.body;
 
-            // refer to document.body node if it is the case
-            if (typeof selector === 'string' && selector.toLowerCase() === 'body') selector = document.body;
+        // if selector is given as a string
+        if (typeof selector === 'string')
 
-            // if selector is given as a string
-            if (typeof selector === 'string')
+            // if it seems that we want to *create* an HTML node
+            if (selector.indexOf('<') === 0 && selector.indexOf('>') > 1 && selector.length > 2) {
 
-                // if it seems that we want to *create* an HTML node
-                if (selector.indexOf('<') === 0 && selector.indexOf('>') > 1 && selector.length > 2) {
+                // create a dummy container
+                parent = document.createElement('div');
 
-                    // create a dummy container
-                    parent = document.createElement('div');
+                // set its body to the selector string
+                parent.innerHTML = selector;
 
-                    // set its body to the selector string
-                    parent.innerHTML = selector;
+                // add created node to the elements array
+                elements.push(parent.firstChild);
 
-                    // add created node to the elements array
-                    elements.push(parent.firstChild);
+            // if we want to select elements
+            } else {
 
-                // if we want to select elements
-                } else {
+                // if parent is not given, consider "document" to be the parent
+                if (!parent) parent = document;
 
-                    // if parent is not given, consider "document" to be the parent
-                    if (!parent) parent = document;
+                // if parent is set and is a ZebraJS object, refer to the first DOM element from the set instead
+                else if (typeof parent === 'object' && parent.version) parent = parent[0];
 
-                    // if parent is set and is a ZebraJS object, refer to the first DOM element from the set instead
-                    else if (typeof parent === 'object' && parent.version) parent = parent[0];
+                // if parent is set and is a string, refer to the matching DOM element
+                else if (typeof parent === 'string') parent = document.querySelector(parent);
 
-                    // if parent is set and is a string, refer to the matching DOM element
-                    else if (typeof parent === 'string') parent = document.querySelector(parent);
+                // if the selector is an ID
+                // select the matching element and add it to the elements array
+                if (selector.match(/^\#[^\s]+$/)) elements.push(parent.querySelector(selector));
 
-                    // if the selector is an ID
-                    // select the matching element and add it to the elements array
-                    if (selector.match(/^\#[^\s]+$/)) elements.push(parent.querySelector(selector));
+                // if the "first_only" argument is set
+                else if (first_only)
 
-                    // if the "first_only" argument is set
-                    else if (first_only)
+                    try {
 
-                        // try
-                        try {
+                        // check for pseudo-selectors first
+                        result = _process_pseudo_selector(selector, parent);
 
-                            // select the matching element and add it to the elements array
-                            elements.push(parent.querySelector(selector));
+                        // if a pseudo-selector was used use that as the result
+                        if (result !== null && result.length > 0) elements.push(result[0]);
 
-                        // if something went wrong (not a valid CSS selector)
-                        } catch (e) {
+                        // if no pseudo-selector was used use standard CSS selector
+                        else if (result === null) elements.push(parent.querySelector(selector));
 
-                            // if "console" is available
-                            if (typeof console !== 'undefined' && console.warn)
+                    // if something went wrong (not a valid CSS selector)
+                    } catch (e) {
 
-                                // show the warning there
-                                console.warn('ZebraJS: Invalid selector "' + selector + '"', e.message);
+                        // if "console" is available
+                        if (typeof console !== 'undefined' && console.warn)
 
-                        }
+                            // show the warning there
+                            console.warn('ZebraJS: Invalid selector "' + selector + '"', e.message);
 
-                    // if the "first" argument is not set
-                    else
+                    }
 
-                        // try
-                        try {
+                // if the "first" argument is not set
+                else
 
-                            // select the matching elements and create and add the to the elements array
-                            elements = Array.prototype.slice.call(parent.querySelectorAll(selector));
+                    try {
 
-                        // if something went wrong (not a valid CSS selector)
-                        } catch (e) {
+                        // check for pseudo-selectors first
+                        result = _process_pseudo_selector(selector, parent);
 
-                            // if "console" is available
-                            if (typeof console !== 'undefined' && console.warn)
+                        // if a pseudo-selector was used use that as the result
+                        if (result !== null && result.length > 0) elements = result;
 
-                                // show the warning there
-                                console.warn('ZebraJS: Invalid selector "' + selector + '"', e.message);
+                        // if no pseudo-selector was used use standard CSS selector
+                        else if (result === null) elements = Array.from(parent.querySelectorAll(selector));
 
-                        }
+                    // if something went wrong (not a valid CSS selector)
+                    } catch (e) {
 
-                }
+                        // if "console" is available
+                        if (typeof console !== 'undefined' && console.warn)
 
-            // if selector is the Document object, the Window object, a DOM node or a text node
-            else if (typeof selector === 'object' && (selector instanceof Document || selector instanceof Window || selector instanceof Element || selector instanceof Text))
+                            // show the warning there
+                            console.warn('ZebraJS: Invalid selector "' + selector + '"', e.message);
 
-                // add it to the elements array
-                elements.push(selector);
+                    }
 
-            // if selector is a NodeList (returned by document.querySelectorAll), add items to the elements array
-            else if (selector instanceof NodeList) elements = Array.prototype.slice.call(selector);
+            }
 
-            // if selector is an array of DOM elements, add them to the elements array
-            else if (Array.isArray(selector)) elements = elements.concat(selector);
+        // if selector is the Document object, the Window object, a DOM node or a text node
+        else if (typeof selector === 'object' && (selector instanceof Document || selector instanceof Window || selector instanceof Element || selector instanceof Text))
 
-            // if the selector is a ZebraJS object, simply return it
-            else if (typeof selector === 'object' && selector.version) return selector;
+            // add it to the elements array
+            elements.push(selector);
 
-            // remove undefined values
-            elements = elements.filter(function(value) { return value !== undefined && value !== null; });
+        // if selector is a NodeList (returned by document.querySelectorAll), add items to the elements array
+        else if (selector instanceof NodeList) elements = Array.from(selector);
 
-            // attach all the ZebraJS methods to the elements array (including plugins, if any)
-            for (property in $.fn) elements[property] = $.fn[property];
+        // if selector is an array of DOM elements, add them to the elements array
+        else if (Array.isArray(selector)) elements = [...elements, ...selector];
 
-            // return the elements "array-on-steroids"
-            return elements;
+        // if the selector is a ZebraJS object, simply return it
+        else if (typeof selector === 'object' && selector.version) return selector;
 
-        }
+        // remove undefined values
+        elements = elements.filter(value => value !== undefined && value !== null);
+
+        // attach all the ZebraJS methods to the elements array (including plugins, if any)
+        Object.assign(elements, $.fn);
+
+        // return the elements "array-on-steroids"
+        return elements;
+
+    }
 
     $.fn = {
 
@@ -191,6 +200,7 @@
 
     };
 
+    // import "_pseudo_selectors.js"
     // import "_helpers.js"
     // import "$.ajax.js"
     // import "$.each.js"
@@ -220,6 +230,7 @@
     // import "height.js"
     // import "hide.js"
     // import "html.js"
+    // import "index.js"
     // import "is.js"
     // import "insertAfter.js"
     // import "insertBefore.js"
@@ -239,6 +250,7 @@
     // import "prev.js"
     // import "ready.js"
     // import "remove.js"
+    // import "removeData.js"
     // import "removeClass.js"
     // import "replaceWith.js"
     // import "scrollLeft.js"
@@ -254,6 +266,7 @@
     // import "val.js"
     // import "width.js"
     // import "wrap.js"
+
     // for browsers that do not support Element.matches() or Element.matchesSelector(), but carry support for
     // document.querySelectorAll(), a polyfill exists:
     if (!Element.prototype.matches)
@@ -268,8 +281,8 @@
 
             function(s) {
 
-                var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-                    i = matches.length;
+                const matches = (this.document || this.ownerDocument).querySelectorAll(s);
+                let i = matches.length;
 
                 // eslint-disable-next-line no-empty
                 while (--i >= 0 && matches.item(i) !== this) {}
